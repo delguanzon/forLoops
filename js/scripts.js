9924afac4dfaafeea9874626ec311e837a329094
delguanzon <yodelguanzon@gmail.com>
2022-10-26 13:23:08 -0700

    Display the output on the webpage
    Co-authored-by: Celeste Grobey <celestegrobey@gmail.com>
    Co-authored-by: Kirsten Opstad <kirsten.opstad@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -22,28 +22,31 @@ function countByUp(countByNum, countToNum) {
 
 function handleFormSubmission() {
   // Retrieve count-to number
-  const countTo = document.getElementById("countToNum").value;
+  const countTo = parseInt(document.getElementById("countToNum").value);
   // Retrieve count-by number
-  const countBy = document.getElementById("countByNum").value;
+  const countBy = parseInt(document.getElementById("countByNum").value);
   // Pass nums to bussiness logic
   const output = countByUp(countBy, countTo);
   // Display results
   const p = document.createElement("p");
   //  Count to: #
-  p.append("Count To: " + countTo)
+  p.append("Count To: " + countTo);
+  p.append(document.createElement("br"));
   //  Count by: #
-  p.append("Count By: " + countBy)
-  //  Output: #, #, # ...
-  p.append("Output: " + output)
+  p.append("Count By: " + countBy);
+  p.append(document.createElement("br"));
+    //  Output: #, #, # ...
+  p.append("Output: " + output);
+  p.append(document.createElement("br"));
   // Add p elements to body
   const body = document.querySelector("body");
-  body.append("p");
+  body.append(p);
 };
 
 window.addEventListener("load", function() {
   let form = document.querySelector("form");
   form.addEventListener("submit", function(event) {
-    event.preventDefault;
-      handleFormSubmission();
+    event.preventDefault();
+    handleFormSubmission();
   });
 });
