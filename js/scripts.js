1c0a9912cfdeab49d4ab6e901bae57d4d757a53c
delguanzon <yodelguanzon@gmail.com>
2022-10-26 11:59:23 -0700

    WIP: Display output on the webpage
    Co-authored-by: Celeste Grobey <celestegrobey@gmail.com>
    Co-authored-by: Kirsten Opstad <kirsten.opstad@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,39 @@ function countByUp(countByNum, countToNum) {
   if (countByNum > countToNum) {
     return 0;
   }
+  const outputNums = [];
   for(let i = countByNum; i <= countToNum; i+=countByNum) {
-    console.log(i);
+    outputNums.push(i);
   }  
+  return outputNums;
 }
+
+// UI Logic
+
+function handleFormSubmission() {
+  // Retrieve count-to number
+  const countTo = document.getElementById("countToNum").value;
+  // Retrieve count-by number
+  const countBy = document.getElementById("countByNum").value;
+  // Pass nums to bussiness logic
+  const output = countByUp(countBy, countTo);
+  // Display results
+  const p = document.createElement("p");
+  //  Count to: #
+  p.append("Count To: " + countTo)
+  //  Count by: #
+  p.append("Count By: " + countBy)
+  //  Output: #, #, # ...
+  p.append("Output: " + output)
+  // Add p elements to body
+  const body = document.querySelector("body");
+  body.append("p");
+};
+
+window.addEventListener("load", function() {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault;
+      handleFormSubmission();
+  });
+});
